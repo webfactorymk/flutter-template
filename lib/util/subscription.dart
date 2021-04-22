24b12071c3a22bf4f182d325b305abaf3ac3e324
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2021-04-22 21:18:52 +0200

    Added AsyncSubscription

--- lib/util/subscription.dart
+++ lib/util/subscription.dart
@@ -12,3 +12,19 @@ class Subscription {
     _cancel = null;
   }
 }
+
+/// Service subscription with async cancel. Call [cancel] to unsubscribe.
+class AsyncSubscription {
+  Future<void> Function()? _cancel;
+
+  AsyncSubscription({required Future<void> Function() cancel})
+      : _cancel = cancel;
+
+  /// Cancels the subscription.
+  ///
+  /// Can be called once. Subsequent calls have no effect.
+  Future<void> cancel() async {
+    await _cancel?.call();
+    _cancel = null;
+  }
+}
