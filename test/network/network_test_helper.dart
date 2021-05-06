04cd4f36df04fdd33f38a6cb984969920cf2a9aa
WF | Ilija Petrov <ilija.petrov@webfactory.mk>
2021-05-06 14:55:55 +0200

    Change refreshToken value in network_test_helper.dart

--- test/network/network_test_helper.dart
+++ test/network/network_test_helper.dart
@@ -4,13 +4,15 @@ import 'package:flutter_template/model/user/user.dart';
 import 'package:flutter_template/model/user/user_credentials.dart';
 
 class NetworkTestHelper {
+  // Valid until 4th of May 2080.
   static final String validToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjM0ODIwNTAzNzcsImlhdCI6MTYyMDEzMDM3N30.Hk3l8Ro7W8tLg8u5MJ2JToj7g67t8kgirtsu5ajhAL4';
   static final String expiredToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODg1OTQzNzcsImlhdCI6MTU1Njk3MTk3N30.fkshAmwlsMGQ4jAFw4Axfoyl6VOcVntSB5XDfdDO9b0';
 
-  static final RefreshToken validRefreshToken =
-      RefreshToken(validToken, 99999999999999);
+  static final RefreshToken validRefreshToken = RefreshToken(
+      validToken,
+      DateTime.now().millisecondsSinceEpoch * 5);
 
   static final Credentials validCredentials =
       Credentials(validToken, validRefreshToken);
