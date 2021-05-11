bd862dff019c891905f71652fa01238d61b555be
Gordana Badarovska <gordana.badarovska@webfactory.mk>
2021-05-11 13:19:35 +0200

    Implements platform communication on iOS

---
+++ ios/Runner/PlatformCommunication/PlatformConstants.swift
@@ -0,0 +1,21 @@
+//
+//  PlatformConstants.swift
+//  Runner
+//
+//  Created by WF | Gordana Badarovska on 11.5.21.
+//
+
+import Foundation
+
+struct Constants {
+    struct Channel {
+        static let name = "com.my-app.package-name.general"
+    }
+    
+    struct Method {
+        // For testing only
+        static let platformTestMethod = "platformTestMethod"
+        static let platformTestMethod2 = "platformTestMethod2"
+        static let nativeLogs = "nativeLogs"
+    }
+}
