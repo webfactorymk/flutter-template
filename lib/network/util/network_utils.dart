bbdf7030a2661289b733f6ee484c217aac60379e
WF | Ilija Petrov <ilija.petrov@webfactory.mk>
2021-05-17 14:48:16 +0200

    Add network conectivity listener add/remove methods

--- lib/network/util/network_utils.dart
+++ lib/network/util/network_utils.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:connectivity/connectivity.dart';
+
+typedef Future<void> _ConnectivityStatusListener(ConnectivityResult result);
 
 /// NetworkUtils class.
 ///
 /// To obtain instance use serviceLocator.get<NetworkUtils>()
 class NetworkUtils {
   late Connectivity _connectivity;
+  late StreamSubscription<ConnectivityResult>? _connectivitySubscription;
 
   NetworkUtils(Connectivity connectivity) {
     _connectivity = connectivity;
@@ -15,6 +20,16 @@ class NetworkUtils {
     return result != ConnectivityResult.none;
   }
 
-//TODO: Add listener for network status changes if needed
-//TODO: with _connectivity.onConnectivityChanged.listen()
+  void addConnectivityListener(
+      _ConnectivityStatusListener connectivityStatusListener) {
+    if (_connectivitySubscription != null) removeConnectivityListener();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(connectivityStatusListener);
+  }
+
+  void removeConnectivityListener() {
+    _connectivitySubscription!.cancel();
+    _connectivitySubscription = null;
+  }
 }
