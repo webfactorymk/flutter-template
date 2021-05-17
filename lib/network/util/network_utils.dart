23893180c6c942c55863261043fc1e16936fae54
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2021-05-17 15:04:37 +0200

    Refactor NetworkUtils

--- lib/network/util/network_utils.dart
+++ lib/network/util/network_utils.dart
@@ -2,34 +2,19 @@ import 'dart:async';
 
 import 'package:connectivity/connectivity.dart';
 
-typedef Future<void> _ConnectivityStatusListener(ConnectivityResult result);
-
-/// NetworkUtils class.
+/// Network connectivity utils.
 ///
 /// To obtain instance use serviceLocator.get<NetworkUtils>()
 class NetworkUtils {
-  late Connectivity _connectivity;
-  late StreamSubscription<ConnectivityResult>? _connectivitySubscription;
+  final Connectivity _connectivity;
 
-  NetworkUtils(Connectivity connectivity) {
-    _connectivity = connectivity;
-  }
+  NetworkUtils(this._connectivity);
+
+  Stream<ConnectivityResult> get connectionUpdates =>
+      _connectivity.onConnectivityChanged;
 
   Future<bool> isConnected() async {
     var result = await _connectivity.checkConnectivity();
     return result != ConnectivityResult.none;
-  }
-
-  void addConnectivityListener(
-      _ConnectivityStatusListener connectivityStatusListener) {
-    if (_connectivitySubscription != null) removeConnectivityListener();
-
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(connectivityStatusListener);
-  }
-
-  void removeConnectivityListener() {
-    _connectivitySubscription!.cancel();
-    _connectivitySubscription = null;
   }
 }
