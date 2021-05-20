e98e12c97bdad88ad5a6da78cf222b7ce80da6e9
WF | Teodora Stefanovska <teodora.stefanovska@webfactory.mk>
2021-05-20 10:48:50 +0200

    Added buildVersion overlay to app file, removed unnecessary RouteInformationParser

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,11 +13,11 @@ import 'package:flutter_template/network/user_api_service.dart';
 import 'package:flutter_template/platform_comm/platform_comm.dart';
 import 'package:flutter_template/resources/strings.dart';
 import 'package:flutter_template/routing/back_button_dispatcher.dart';
-import 'package:flutter_template/routing/global_route_information_parser.dart';
 import 'package:flutter_template/routing/global_router_delegate.dart';
 import 'package:flutter_template/routing/home_state.dart';
 import 'package:flutter_template/user/user_manager.dart';
 import 'package:flutter_template/util/app_lifecycle_observer.dart';
+import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -35,12 +37,24 @@ class App extends StatefulWidget {
 //todo replace state with flutter hooks
 
 class _AppState extends State<App> {
+  String _buildVersion = '';
+
+  Future<void> _getBuildVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    setState(() {
+      _buildVersion =
+          'Build version ${packageInfo.version} (${packageInfo.buildNumber})';
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     serviceLocator.get<AppLifecycleObserver>().activate();
 
     if (!FlavorConfig.isProduction()) {
+      _getBuildVersion();
       serviceLocator
           .get<PlatformComm>()
           .echoMessage('echo')
@@ -58,6 +72,15 @@ class _AppState extends State<App> {
   void dispose() {
     serviceLocator.get<AppLifecycleObserver>().deactivate();
     super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!FlavorConfig.isProduction()) {
+      WidgetsBinding.instance
+          ?.addPostFrameCallback((_) => _insertOverlay(context, _buildVersion));
+    }
   }
 
   @override
@@ -98,7 +121,8 @@ class _AppState extends State<App> {
             return signUpCubit;
           }),
         ],
-        child: MaterialApp.router(
+        child: MaterialApp(
+          navigatorKey: navigatorKey,
           theme: ThemeData(
             brightness: Brightness.light,
             primarySwatch: Colors.blue,
@@ -117,11 +141,41 @@ class _AppState extends State<App> {
             const Locale('en'), // English
             const Locale('mk'), // Macedonian
           ],
-          routeInformationParser: AppRouteInformationParser(),
-          routerDelegate: _routerDelegate,
-          backButtonDispatcher: _backButtonDispatcher,
+          home: Router(
+              routerDelegate: _routerDelegate,
+              backButtonDispatcher: _backButtonDispatcher),
         ),
       ),
+    );
+  }
+
+  void _insertOverlay(BuildContext context, buildVersion) {
+    Overlay.of(context)?.insert(
+      OverlayEntry(builder: (context) {
+        var safePadding = MediaQuery.of(context).padding.bottom;
+        final size = MediaQuery.of(context).size;
+        final textSize = (TextPainter(
+                text: TextSpan(
+                    text: buildVersion,
+                    style: Theme.of(context).textTheme.caption),
+                maxLines: 1,
+                textScaleFactor: MediaQuery.of(context).textScaleFactor,
+                textDirection: TextDirection.ltr)
+              ..layout())
+            .size;
+
+        return Positioned(
+          height: 56,
+          top: size.height - max(safePadding, 20),
+          left: (size.width - textSize.width) / 2,
+          child: IgnorePointer(
+            child: Text(
+              buildVersion,
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ),
+        );
+      }),
     );
   }
 }
--- lib/routing/global_router_delegate.dart
+++ lib/routing/global_router_delegate.dart
@@ -1,13 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_template/config/flavor_config.dart';
 import 'package:flutter_template/feature/auth/global_handler/global_auth_cubit.dart';
 import 'package:flutter_template/feature/auth/global_handler/global_auth_state.dart';
 import 'package:flutter_template/routing/auth_state.dart';
-import 'package:package_info/package_info.dart';
 
 import 'app_route_path.dart';
 import 'pages/pages.dart';
@@ -18,20 +14,7 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
   late AuthState authState;
   GlobalAuthState globalAuthState;
 
-  String _buildVersion = '';
-
-  AppRouterDelegate() : globalAuthState = AuthenticationFailure() {
-    if (!FlavorConfig.isProduction()) {
-      _getBuildVersion();
-    }
-  }
-
-  Future<void> _getBuildVersion() async {
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
-    _buildVersion =
-        'Build version ${packageInfo.version} (${packageInfo.buildNumber})';
-  }
+  AppRouterDelegate() : globalAuthState = AuthenticationFailure();
 
   List<Page> _getPages(GlobalAuthState state) {
     List<Page> pages = [];
@@ -53,10 +36,6 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
     return BlocBuilder(
       bloc: authCubit,
       builder: (BuildContext context, GlobalAuthState state) {
-        if (!FlavorConfig.isProduction()) {
-          WidgetsBinding.instance?.addPostFrameCallback(
-              (_) => _insertOverlay(context, _buildVersion));
-        }
         globalAuthState = state;
         return Navigator(
           key: navigatorKey,
@@ -76,35 +55,5 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
   @override
   Future<void> setNewRoutePath(AppRoutePath configuration) async {
     notifyListeners();
-  }
-
-  void _insertOverlay(BuildContext context, buildVersion) {
-    Overlay.of(context)?.insert(
-      OverlayEntry(builder: (context) {
-        var safePadding = MediaQuery.of(context).padding.bottom;
-        final size = MediaQuery.of(context).size;
-        final textSize = (TextPainter(
-                text: TextSpan(
-                    text: buildVersion,
-                    style: Theme.of(context).textTheme.caption),
-                maxLines: 1,
-                textScaleFactor: MediaQuery.of(context).textScaleFactor,
-                textDirection: TextDirection.ltr)
-              ..layout())
-            .size;
-
-        return Positioned(
-          height: 56,
-          top: size.height - max(safePadding, 20),
-          left: (size.width - textSize.width) / 2,
-          child: IgnorePointer(
-            child: Text(
-              buildVersion,
-              style: Theme.of(context).textTheme.caption,
-            ),
-          ),
-        );
-      }),
-    );
   }
 }
