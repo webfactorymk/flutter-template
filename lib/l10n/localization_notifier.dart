52bfb798209f3b63021be38ce05c9a3bfa2bfe64
WF | Ilija Petrov <ilija.petrov@webfactory.mk>
2021-05-31 16:03:40 +0200

    Add LocalizationNotifier

---
+++ lib/l10n/localization_notifier.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/l10n/l10n.dart';
+
+class LocalizationNotifier extends ChangeNotifier {
+  Locale _locale = L10n.getLocale('en');
+
+  Locale get locale => _locale;
+
+  void setLocale(Locale locale) {
+    _locale = locale;
+    notifyListeners();
+  }
+
+  void clearLocale() {
+    _locale = L10n.getLocale('en');
+    notifyListeners();
+  }
+}
