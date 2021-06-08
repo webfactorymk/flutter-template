ac49f78caf544b43dea2b26ebc19ea41ec20dce7
WF | Ilija Petrov <ilija.petrov@webfactory.mk>
2021-06-08 11:54:10 +0200

    Save the preferred language and theme in their change notifiers

--- lib/resources/localization/localization_notifier.dart
+++ lib/resources/localization/localization_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/resources/localization/l10n.dart';
+import 'package:flutter_template/util/preferences.dart';
 
 class LocalizationNotifier extends ChangeNotifier {
   late Locale _locale;
@@ -11,11 +12,13 @@ class LocalizationNotifier extends ChangeNotifier {
 
   void setLocale(String languageCode) {
     _locale = L10n.getLocale(languageCode);
+    saveCurrentLanguage(_locale.languageCode);
     notifyListeners();
   }
 
   void clearLocale() {
     _locale = L10n.getLocale('en');
+    saveCurrentLanguage(_locale.languageCode);
     notifyListeners();
   }
 }
--- lib/resources/theme/theme_change_notifier.dart
+++ lib/resources/theme/theme_change_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/util/preferences.dart';
 
 class ThemeChangeNotifier extends ChangeNotifier {
   bool _isDarkTheme;
@@ -8,14 +9,14 @@ class ThemeChangeNotifier extends ChangeNotifier {
 
   ThemeChangeNotifier.lightTheme() : _isDarkTheme = false;
 
-  ThemeChangeNotifier.systemTheme(BuildContext context) :
-    _isDarkTheme = MediaQuery.platformBrightnessOf(context) == Brightness.dark;
+  ThemeChangeNotifier.systemTheme(BuildContext context)
+      : _isDarkTheme =
+            MediaQuery.platformBrightnessOf(context) == Brightness.dark;
 
-  ThemeChangeNotifier.fromThemeMode(BuildContext context, ThemeMode themeMode) :
-    _isDarkTheme = themeMode == ThemeMode.dark ||
-        (themeMode == ThemeMode.system &&
-            MediaQuery.platformBrightnessOf(context) == Brightness.dark);
-
+  ThemeChangeNotifier.fromThemeMode(BuildContext context, ThemeMode themeMode)
+      : _isDarkTheme = themeMode == ThemeMode.dark ||
+            (themeMode == ThemeMode.system &&
+                MediaQuery.platformBrightnessOf(context) == Brightness.dark);
 
   bool get isDarkTheme => _isDarkTheme;
 
@@ -23,17 +24,20 @@ class ThemeChangeNotifier extends ChangeNotifier {
 
   void setDarkTheme() {
     _isDarkTheme = true;
+    saveIsDarkThemePreferred(_isDarkTheme);
     notifyListeners();
   }
 
   void setLightTheme() {
     _isDarkTheme = false;
+    saveIsDarkThemePreferred(_isDarkTheme);
     notifyListeners();
   }
 
   /// Toggles the current theme value. Returns `true` if dark is the new theme.
   bool toggleTheme() {
     _isDarkTheme = !_isDarkTheme;
+    saveIsDarkThemePreferred(_isDarkTheme);
     notifyListeners();
     return _isDarkTheme;
   }
