eb7b3adc5e437a03261a6dd0326912ea6f3643b7
WF | Teodora Stefanovska <teodora.stefanovska@webfactory.mk>
2021-06-08 09:22:15 +0200

    Reordered text styles constants (font-size ascending)

--- lib/resources/styles/text_styles.dart
+++ lib/resources/styles/text_styles.dart
@@ -1,41 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/resources/colors/color_palette.dart';
 
-const kAppBarTextStyle = TextStyle(
-  fontSize: 17.0,
-  color: Colors.black,
+const kSmallErrorTextStyle = TextStyle(
+  fontSize: 10.0,
+  color: Colors.red,
 );
 
-const kAppBarWhiteTextStyle = TextStyle(
-  fontSize: 17.0,
-  color: Colors.white,
+const kGreyTextStyle = TextStyle(
+  color: ColorPalette.textGray,
+  fontSize: 12.0,
 );
 
-const kPrimaryDarkHeadingTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.primaryDark,
-  fontWeight: FontWeight.bold,
-);
-
-const kPrimaryDarkLargeHeadingTextStyle = TextStyle(
-  fontSize: 30.0,
-  color: ColorPalette.primaryDark,
-  fontWeight: FontWeight.bold,
-);
-
-const kPrimaryDarkTextStyle = TextStyle(
-  fontSize: 15.0,
-  color: ColorPalette.primaryDark,
-);
-
-const kPrimaryWhiteTextStyle = TextStyle(
-  fontSize: 15.0,
+const kWhiteSmallTextStyle = TextStyle(
   color: ColorPalette.white,
+  fontSize: 12.0,
 );
 
-const kPrimaryDarkBoldTextStyle = TextStyle(
-  fontSize: 15.0,
-  color: ColorPalette.primaryDark,
+const kMediumErrorTextStyle = TextStyle(
+  fontSize: 12.0,
+  color: Colors.red,
+);
+
+const kPrimaryLightSmallTextStyle = TextStyle(
+  fontSize: 13.0,
+  color: ColorPalette.primaryLight,
+);
+
+const kWhiteSmallBoldTextStyle = TextStyle(
+  fontSize: 13.0,
+  color: Colors.white,
   fontWeight: FontWeight.bold,
 );
 
@@ -50,9 +43,118 @@ const kPrimarySmallTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
+const kAccentBoldSmallTextStyle = TextStyle(
+  fontSize: 13.0,
+  color: ColorPalette.accent,
+  fontWeight: FontWeight.bold,
+);
+
+const kAccentBoldTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: ColorPalette.accent,
+  fontWeight: FontWeight.bold,
+);
+
+const kAccentTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: ColorPalette.accent,
+);
+
+const kBlackSmallTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: ColorPalette.black,
+);
+
+const kBlackBoldSmallTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kPrimaryDarkBoldTextStyle = TextStyle(
+  fontSize: 15.0,
+  color: ColorPalette.primaryDark,
+  fontWeight: FontWeight.bold,
+);
+
+const kPrimaryWhiteTextStyle = TextStyle(
+  fontSize: 15.0,
+  color: ColorPalette.white,
+);
+
+const kPrimaryDarkTextStyle = TextStyle(
+  fontSize: 15.0,
+  color: ColorPalette.primaryDark,
+);
+
+const kInputFieldTextStyle = TextStyle(
+  fontSize: 15.0,
+  color: ColorPalette.primary,
+  fontWeight: FontWeight.bold,
+);
+
 const kDescriptionGrayTextStyle = TextStyle(
   fontSize: 16.0,
   color: ColorPalette.textGray,
+);
+
+const kMediumWhiteButtonTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kPrimaryTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: ColorPalette.primary,
+);
+
+const kPrimaryBoldTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: ColorPalette.primary,
+  fontWeight: FontWeight.bold,
+);
+
+const kMediumAccentTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: ColorPalette.accent,
+);
+
+const kBlackTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: ColorPalette.black,
+);
+
+const kWhiteTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: ColorPalette.white,
+);
+
+const kProfileDetailTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kAppBarTextStyle = TextStyle(
+  fontSize: 17.0,
+  color: Colors.black,
+);
+
+const kAppBarWhiteTextStyle = TextStyle(
+  fontSize: 17.0,
+  color: Colors.white,
+);
+
+const kBlackBoldNameTextStyle = TextStyle(
+  fontSize: 17.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kPrimaryDarkHeadingTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.primaryDark,
+  fontWeight: FontWeight.bold,
 );
 
 const kButtonTextStyle = TextStyle(
@@ -61,9 +163,74 @@ const kButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
-const kMediumWhiteButtonTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: Colors.white,
+const kBlackBoldTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kBlackBoldHeadingTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kAccentButtonTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.accent,
+  fontWeight: FontWeight.bold,
+);
+
+const kWhiteBoldTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kWhiteBoldNameTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: ColorPalette.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kPrimaryLargeTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: ColorPalette.primary,
+);
+
+const kPrimaryLargeBoldTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: ColorPalette.primary,
+  fontWeight: FontWeight.bold,
+);
+
+const kBlackLargeBoldTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kBlackTitleTextStyle = TextStyle(
+  fontSize: 24.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.w900,
+);
+
+const kWhiteBoldLargeTextStyle = TextStyle(
+  fontSize: 24.0,
+  color: ColorPalette.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kBlackNameHeadingTextStyle = TextStyle(
+  fontSize: 24.0,
+  color: ColorPalette.black,
+  fontWeight: FontWeight.w900,
+);
+
+const kPrimaryDarkLargeHeadingTextStyle = TextStyle(
+  fontSize: 30.0,
+  color: ColorPalette.primaryDark,
   fontWeight: FontWeight.bold,
 );
 
@@ -85,133 +252,6 @@ const kBlackHeadingTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
-const kBlackTitleTextStyle = TextStyle(
-  fontWeight: FontWeight.w900,
-  color: ColorPalette.black,
-  fontSize: 24.0,
-);
-
-const kPrimaryTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: ColorPalette.primary,
-);
-
-const kPrimaryBoldTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: ColorPalette.primary,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackSmallTextStyle = TextStyle(
-  fontSize: 14.0,
-  color: ColorPalette.black,
-);
-const kBlackBoldSmallTextStyle = TextStyle(
-  fontSize: 14.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: ColorPalette.black,
-);
-
-const kGreyTextStyle = TextStyle(
-  color: ColorPalette.textGray,
-  fontSize: 12.0,
-);
-
-const kWhiteSmallTextStyle = TextStyle(
-  color: ColorPalette.white,
-  fontSize: 12.0,
-);
-
-const kBlackBoldTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.bold,
-);
-
-const kPrimaryLargeTextStyle = TextStyle(
-  fontSize: 22.0,
-  color: ColorPalette.primary,
-);
-
-const kPrimaryLargeBoldTextStyle = TextStyle(
-  fontSize: 22.0,
-  color: ColorPalette.primary,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackLargeBoldTextStyle = TextStyle(
-  fontSize: 22.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackBoldHeadingTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.bold,
-);
-
-const kAccentButtonTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.accent,
-  fontWeight: FontWeight.bold,
-);
-
-const kWhiteSmallBoldTextStyle = TextStyle(
-  fontSize: 13.0,
-  color: Colors.white,
-  fontWeight: FontWeight.bold,
-);
-
-const kAccentTextStyle = TextStyle(
-  fontSize: 14.0,
-  color: ColorPalette.accent,
-);
-
-const kMediumAccentTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: ColorPalette.accent,
-);
-
-const kAccentBoldSmallTextStyle = TextStyle(
-  fontSize: 13.0,
-  color: ColorPalette.accent,
-  fontWeight: FontWeight.bold,
-);
-
-const kAccentBoldTextStyle = TextStyle(
-  fontSize: 14.0,
-  color: ColorPalette.accent,
-  fontWeight: FontWeight.bold,
-);
-
-const kWhiteTextStyle = TextStyle(
-  fontSize: 16.0,
-  color: ColorPalette.white,
-);
-
-const kWhiteBoldTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.white,
-  fontWeight: FontWeight.bold,
-);
-
-const kWhiteBoldLargeTextStyle = TextStyle(
-  fontSize: 24.0,
-  color: ColorPalette.white,
-  fontWeight: FontWeight.bold,
-);
-
-const kProfileDetailTextStyle = TextStyle(
-  fontSize: 16,
-  fontWeight: FontWeight.bold,
-);
-
 const kWhiteHeaderBoldTextStyle = TextStyle(
   fontSize: 30.0,
   color: ColorPalette.white,
@@ -222,43 +262,4 @@ const kWhiteHeaderTextStyle = TextStyle(
   fontSize: 30.0,
   color: ColorPalette.white,
   fontWeight: FontWeight.w900,
-);
-
-const kInputFieldTextStyle = TextStyle(
-  fontSize: 15.0,
-  color: ColorPalette.primary,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackBoldNameTextStyle = TextStyle(
-  fontSize: 17.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.bold,
-);
-
-const kBlackNameHeadingTextStyle = TextStyle(
-  fontSize: 24.0,
-  color: ColorPalette.black,
-  fontWeight: FontWeight.w900,
-);
-
-const kPrimaryLightSmallTextStyle = TextStyle(
-  fontSize: 13.0,
-  color: ColorPalette.primaryLight,
-);
-
-const kWhiteBoldNameTextStyle = TextStyle(
-  fontSize: 20.0,
-  color: ColorPalette.white,
-  fontWeight: FontWeight.bold,
-);
-
-const kMediumErrorTextStyle = TextStyle(
-  fontSize: 12.0,
-  color: Colors.red,
-);
-
-const kSmallErrorTextStyle = TextStyle(
-  fontSize: 10.0,
-  color: Colors.red,
 );
