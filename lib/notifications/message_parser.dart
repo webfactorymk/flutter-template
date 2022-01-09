f38d2ddfb150254ffe78b496bf1e5e03dc9c8fb6
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2022-01-10 00:44:02 +0100

    Add MultiMessageParser

--- lib/notifications/message_parser.dart
+++ lib/notifications/message_parser.dart
@@ -9,6 +9,35 @@ abstract class MessageParser {
   Message parseMessage(dynamic remoteData);
 }
 
+/// Message parser that will use a separate parser for each message type.
+abstract class MultiMessageParser implements MessageParser {
+  final Map<String, MessageParser> _messageParserForType = Map();
+
+  /// Registers a [MessageParser] for a specific [Message.type]
+  void registerMessageParser({
+    required MessageParser parser,
+    required Iterable<String> forMessageTypes,
+  }) {
+    forMessageTypes.forEach((type) {
+      _messageParserForType[type] = parser;
+    });
+  }
+
+  /// Determines the [Message.type] from raw remote notification data.
+  String getTypeFromRawMessage(dynamic remoteData);
+
+  @override
+  Message parseMessage(remoteData) {
+    final type = getTypeFromRawMessage(remoteData);
+    final parser = _messageParserForType[type];
+    if (parser == null) {
+      throw Exception('NotificationsManager - '
+          'Message type does not have a parser: $type');
+    }
+    return parser.parseMessage(remoteData);
+  }
+}
+
 /// Message parser that expects a [Message] as param and just forwards it.
 class StubMessageParser implements MessageParser {
   @override
---
+++ test/notifications/notification_multi_parser_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_template/notifications/message.dart';
+import 'package:flutter_template/notifications/message_parser.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'notification_multi_parser_test.mocks.dart';
+
+class TestMultiMessageParser extends MultiMessageParser {
+  @override
+  String getTypeFromRawMessage(remoteData) => remoteData;
+}
+
+@GenerateMocks([MessageParser])
+void main() {
+  late MultiMessageParser parser;
+
+  setUp(() {
+    parser = TestMultiMessageParser();
+  });
+
+  test('No parser for message', () {
+    expect(() => parser.parseMessage('unknown'), throwsA(isA<Exception>()));
+  });
+
+  test('Multiple parsers for message types', () {
+    MockMessageParser parserA = MockMessageParser();
+    MockMessageParser parserB = MockMessageParser();
+    MockMessageParser parserCD = MockMessageParser();
+
+    when(parserA.parseMessage('A')).thenReturn(Message('A'));
+    when(parserB.parseMessage('B')).thenReturn(Message('B'));
+    when(parserCD.parseMessage('C')).thenReturn(Message('C'));
+    when(parserCD.parseMessage('D')).thenReturn(Message('D'));
+
+    parser
+      ..registerMessageParser(parser: parserA, forMessageTypes: ['A'])
+      ..registerMessageParser(parser: parserB, forMessageTypes: ['B'])
+      ..registerMessageParser(parser: parserCD, forMessageTypes: ['C', 'D']);
+
+    parser.parseMessage('A');
+    parser.parseMessage('B');
+    parser.parseMessage('C');
+    parser.parseMessage('D');
+
+    verifyInOrder([
+      parserA.parseMessage('A'),
+      parserB.parseMessage('B'),
+      parserCD.parseMessage('C'),
+      parserCD.parseMessage('D'),
+    ]);
+  });
+}
