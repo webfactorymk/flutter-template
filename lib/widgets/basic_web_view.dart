a1d603669f9a6c838bc1911261b7a3a1942e8f79
Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2023-01-22 01:40:41 +0100

    Migrate webview to 4.0.2

--- lib/widgets/basic_web_view.dart
+++ lib/widgets/basic_web_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/feature/loading/ui/circular_progress_indicator.dart';
+import 'package:flutter_template/log/log.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class BasicWebView extends StatefulWidget {
@@ -16,21 +17,53 @@ class BasicWebView extends StatefulWidget {
 }
 
 class _BasicWebViewState extends State<BasicWebView> {
-  bool isLoading = true;
   final _key = UniqueKey();
+  late final WebViewController _controller;
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.transparent)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            Log.e(error.toString());
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            // if (request.url.startsWith('https://www.youtube.com/')) {
+            //   return NavigationDecision.prevent;
+            // }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Stack(
         children: <Widget>[
-          WebView(
-            backgroundColor: Colors.transparent,
-            key: _key,
-            initialUrl: widget.url,
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: onPageFinished,
-          ),
+          WebViewWidget(key: _key, controller: _controller),
           Visibility(
             visible: isLoading,
             child: Center(
@@ -40,11 +73,5 @@ class _BasicWebViewState extends State<BasicWebView> {
         ],
       ),
     );
-  }
-
-  void onPageFinished(String url) {
-    setState(() {
-      isLoading = false;
-    });
   }
 }
