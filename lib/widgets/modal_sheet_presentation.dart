f2d9d8ce1d4ad73fbcc06c336da3db54347844ed
Sasha Krstev <sasha@webfactory.mk>
2023-07-07 10:11:55 +0200

    Hide Modal bottom sheet from material library #61

--- lib/widgets/modal_sheet_presentation.dart
+++ lib/widgets/modal_sheet_presentation.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 Future<void> showModalSheet(
